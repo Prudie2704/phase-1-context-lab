c1be6083960facbcd2d4cbcd6f19a017d45d59ee
prudence <prudence.jerono@student.moringaschool.com>
2023-03-31 17:05:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,80 @@
 /* Your Code Here */
+const helpers = require('./helpers')
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  
+  function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map(row => createEmployeeRecord(row))
+  }
+  
+  function createTimeInEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ')
+    this.timeInEvents.push({
+      type: 'TimeIn',
+      date,
+      hour: parseInt(hour, 10)
+    })
+    return this
+  }
+  
+  function createTimeOutEvent(dateTimeString) {
+    const [date, hour] = dateTimeString.split(' ')
+    this.timeOutEvents.push({
+      type: 'TimeOut',
+      date,
+      hour: parseInt(hour, 10)
+    })
+    return this
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find(event => event.date === date)
+    const timeOut = employee.timeOutEvents.find(event => event.date === date)
+    return (timeOut.hour - timeIn.hour) / 100
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date)
+    return hoursWorked * employee.payPerHour
+  }
+  
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map(event => event.date)
+    const wages = datesWorked.map(date => wagesEarnedOnDate(employee, date))
+    return wages.reduce((total, wage) => total + wage, 0)
+  }
+  
+  function calculatePayroll(employees) {
+    const wages = employees.map(employee => allWagesFor(employee))
+    return wages.reduce((total, wage) => total + wage, 0)
+  }
+  
+  function findEmployeeByFirstName(employees, firstName) {
+    return employees.find(employee => employee.firstName === firstName)
+  }
+  
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll,
+    findEmployeeByFirstName
+  }
+  
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +96,5 @@ const allWagesFor = function () {
 
     return payable
 }
+
 
